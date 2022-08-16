d410c1da187aeb7ede86be451e87a3d5ae1c650e
andrian.dwiari <andrian@globalpromedika.co.id>
2022-08-16 16:38:59 +0700

    update template: - page login - page user outlet

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,51 +1,71 @@
-@section('title')
-    Login
-@endsection
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8" />
-<title>@yield('title') | GPS Attendance</title>
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta name="description" content="" />
-<meta name="author" content="" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login | GPS Attendance</title>
 
-<link href="{{asset('/assets/css/vendor.min.css')}}" rel="stylesheet" />
-<link href="{{asset('/assets/css/app.min.css')}}" rel="stylesheet" />
-<style>
-    .form-login{
-        background: #fff;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: 2px 2px 2px #ddd;
-        border: 1px solid #ddd;
-    }
-</style>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('/assets/AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{asset('/assets/AdminLTE-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('/assets/AdminLTE-3.2.0/dist/css/adminlte.min.css')}}">
 </head>
-<body style="height:0 !important">
-
-    <div id="container" style="margin-top:200px !important">
-        <div class="col-md-4 offset-md-4 form-login">
-            <form action="{{route('action.login')}}" method="post" accept-charset="utf-8">
-                @csrf
-                <h2 class="text-center">Login</h2>
-                <div class="input-group mb-2">
-                    <span class="input-group-text">@</span>
-                    <input type="email" name="email" class="form-control" required />
-                </div>
-                <div class="input-group">
-                    <span class="input-group-text"><i class="fa-solid fa-lock"></i>
-                    </span>
-                    <input type="password" name="password" class="form-control" required />
-                </div>
-                <button type="submit" class="btn btn-primary mt-2">Login</button>
-            </form>
-        </div>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="{{route('login')}}" class="h1"><b>Login</b></a>
     </div>
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
 
+      <form action="{{route('action.login')}}" method="post">
+        @csrf
+        <div class="input-group mb-3">
+          <input class="form-control" placeholder="Email" type="email" name="email" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input class="form-control" placeholder="Password" type="password" name="password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            &nbsp;
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
 
-<script data-cfasync="false" src="{{asset('/assets/email-decode.min.js')}}"></script>
-<script src="{{asset('/assets/js/vendor.min.js')}}"></script>
-<script src="{{asset('/assets/js/app.min.js')}}"></script>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('/assets/AdminLTE-3.2.0/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,6 +9,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title') | GPS HRMS</title>
 
+  <meta name="csrf-token" content="{{csrf_token()}}"/>
+
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
