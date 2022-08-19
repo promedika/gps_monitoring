b8c7db7329befa43e197d8633778263d4af7001a
andrian.dwiari <andrian@globalpromedika.co.id>
2022-08-19 09:20:30 +0700

    blade: update useroutlet

--- resources/views/useroutlet/index.blade.php
+++ resources/views/useroutlet/index.blade.php
@@ -3,76 +3,72 @@
     User Outlet
 @endsection
 @section('content')
-<!-- Content Header (Page header) -->
-<div class="content-header">
-  <div class="container-fluid">
-    <div class="row mb-2">
-      <div class="col-sm-6">
-        <h1 class="m-0">Users Outlet</h1>
-      </div><!-- /.col -->
-      <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}">Home</a></li>
-          <li class="breadcrumb-item active">Users Outlet</li>
-        </ol>
-      </div><!-- /.col -->
-    </div><!-- /.row -->
-  </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
-
-<!-- Main content -->
-<section class="content">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                  <div class="card-header">
-                    <a href="#" title="Add" class="btn btn-primary btn-block col-2 btn-add-useroutlet"><i class="fa solid fa-plus"></i> Create New</a>
-                  </div>
-                  <!-- /.card-header -->
-                  <div class="card-body p-0">
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th>ID</th>
-                          <th>Name</th>
-                          <th>Outlet Name</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach ($useroutlets as $useroutlet)
-                        <tr>
-                          <td>{{$useroutlet->id}}</td>
-                          <td>{{$useroutlet->name}}</td>
-                          <td>{{$useroutlet->outlet->name}}</td>
-                          <td>
-                            <div class="row">
-                                <div class="col-2">
-                                    <a href="#" useroutlet-id="{{$useroutlet->id}}" title="Edit" class="btn btn-block bg-gradient-warning btn-sm btn-edit-useroutlet"><i class="fas fa-edit"></i></a>
-                                </div>
-
-                                <div class="col-2">
-                                    <a href="#" useroutlet-id="{{$useroutlet->id}}" title="Delete" class="btn btn-block bg-gradient-danger btn-sm btn-delete-useroutlet"><i class="fas fa-trash"></i></a>
-                                </div>
-                            </div>
-                          </td>
-                        </tr>
-                        @endforeach
-                      </tbody>
-                    </table>
-                  </div>
-                  <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-            </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Users Outlet</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}">Home</a></li>
+            <li class="breadcrumb-item active">Users Outlet</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
     </div><!-- /.container-fluid -->
-</section>
-<!-- /.content -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <a href="#" title="Add" class="btn btn-primary btn-block col-2 btn-add-useroutlet"><i class="fa solid fa-plus"></i></a>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-0">
+                      <table class="table">
+                        <thead>
+                          <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Outlet Name</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($useroutlets as $useroutlet)
+                          <tr>
+                            <td>{{$useroutlet->id}}</td>
+                            <td>{{$useroutlet->name}}</td>
+                            <td>{{$useroutlet->outlet->name}}</td>
+                            <td>
+                              <a href="#" useroutlet-id="{{$useroutlet->id}}" title="Edit" class="btn btn-warning btn-edit-useroutlet"><i class="fas fa-edit"></i></a>
+                              <a href="#" useroutlet-id="{{$useroutlet->id}}" title="Delete" class="btn btn-danger btn-delete-useroutlet"><i class="fas fa-trash"></i></a>
+                            </td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+              </div>
+              <!-- /.col -->
+          </div>
+          <!-- /.row -->
+      </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
 
 <!-- The Modal Add -->
 <div class="modal fade in" id="modalCreateUserOutlet" data-backdrop="static" data-keyboard="false">
