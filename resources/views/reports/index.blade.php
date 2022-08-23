82422c1feae915c044a09791030adaa1017834cc
andrian.dwiari <andrian@globalpromedika.co.id>
2022-08-23 09:29:44 +0700

    add blade report

---
+++ resources/views/reports/index.blade.php
@@ -0,0 +1,173 @@
+@extends('master')
+@section('title')
+    Image Attendance
+@endsection
+@section('custom_link_css')
+<link rel="stylesheet" href="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.1.2/css/dataTables.dateTime.min.css">
+@endsection
+@section('content')
+<div class="content-wrapper" style="background: linen">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Attendance Reports</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}">Home</a></li>
+              <li class="breadcrumb-item active">Attendance Reports</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table-bordered">
+                            <tbody><tr>
+                                <td>Start date:</td>
+                                <td><input type="text" id="min" name="min"></td>
+                            </tr>
+                            <tr>
+                                <td>End date:</td>
+                                <td><input type="text" id="max" name="max"></td>
+                            </tr>
+                        </tbody></table>
+                        <table class="table">
+                            <thead>
+                              <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">User</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Work Hours</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Action</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $nomor = 1;
+                                     ?>
+                              @forelse ($posts as $post)
+                                <tr class="data_post">
+                                    <td>{{$nomor++}}</td>
+                                    <td>{{$post->user_fullname}}</td>
+                                    <td>{{str_replace(' 00:00:00','',$post->created_at)}}</td>
+                                    <td>{{$post->work_hour}}</td>
+                                    <td>{{$post->status}}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-block bg-gradient-warning btn-sm">Detail</a>
+                                        </form>
+                                    </td>
+                                </tr>
+                              @empty
+                                  <div class="alert alert-danger">
+                                      Data Post belum Tersedia.
+                                  </div>
+                              @endforelse
+                            </tbody>
+                          </table>  
+                          {{-- {{ $posts->links() }} --}}
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+@endsection
+@section('custom_script_js')
+    <!-- DataTables  & Plugins -->
+<script src="https://cdn.datatables.net/datetime/1.1.2/js/dataTables.dateTime.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/plug-ins/1.12.1/filtering/row-based/range_dates.js"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('/assets/AdminLTE-3.2.0/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>       
+    <script>
+            $.fn.dataTableExt.afnFiltering.push(
+    function( oSettings, aData, iDataIndex ) {
+        var iFini = document.getElementById('fini').value;
+        var iFfin = document.getElementById('ffin').value;
+        var iStartDateCol = 6;
+        var iEndDateCol = 7;
+ 
+        iFini=iFini.substring(6,10) + iFini.substring(3,5)+ iFini.substring(0,2);
+        iFfin=iFfin.substring(6,10) + iFfin.substring(3,5)+ iFfin.substring(0,2);
+ 
+        var datofini=aData[iStartDateCol].substring(6,10) + aData[iStartDateCol].substring(3,5)+ aData[iStartDateCol].substring(0,2);
+        var datoffin=aData[iEndDateCol].substring(6,10) + aData[iEndDateCol].substring(3,5)+ aData[iEndDateCol].substring(0,2);
+ 
+        if ( iFini === "" && iFfin === "" )
+        {
+            return true;
+        }
+        else if ( iFini <= datofini && iFfin === "")
+        {
+            return true;
+        }
+        else if ( iFfin >= datoffin && iFini === "")
+        {
+            return true;
+        }
+        else if (iFini <= datofini && iFfin >= datoffin)
+        {
+            return true;
+        }
+        return false;
+    }
+);
+    </script>
+    {{-- <script>
+        jQuery(document).ready(function () {
+            $('.table-bordered').DataTable();
+
+            jQuery('.data_post').each(function(index, value) {
+                let td_map = jQuery(this).find('.map');
+                let latitude = td_map.data('latitude');
+                let longitude = td_map.data('longitude');
+                console.log(latitude+'|'+longitude);
+
+                var myCenter = new google.maps.LatLng(latitude, longitude);
+                function initialize()
+                {
+                    var mapProp = 
+                    {
+                        center:myCenter,
+                        zoom:19,
+                        mapTypeId:google.maps.MapTypeId.ROADMAP
+                    };
+
+                    // var map = new google.maps.Map(document.getElementById("map"),mapProp);
+                    let map = new google.maps.Map(td_map,mapProp);
+
+                    var marker = new google.maps.Marker({
+                        position:myCenter,
+                        animation:google.maps.Animation.BOUNCE
+                    });
+
+                    marker.setMap(map);
+                }
+                google.maps.event.addDomListener(window, 'load', initialize);
+            });
+
+        });
+</script> --}}
+@endsection
