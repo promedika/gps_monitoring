6467972613ddde8610be7a1466093efbe2c4b2cb
andrian.dwiari <andrian@globalpromedika.co.id>
2022-10-25 10:13:03 +0700

    dump

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -7,49 +7,53 @@ use Stevebauman\Location\Facades\Location;
 
 class HelperController extends Controller
 {
-    public static function getGpsFromIp() {
-        // $ip = \Request::ip();
-        $ip = $this->getIpAddress();
+    // public static function getGpsFromIp() {
+    //     // $ip = \Request::ip();
+    //     $data = \Location::get($ip);
 
+    //     $return = ['ip' => $ip, 'data' => $data];
+    //     dd($return,$_SERVER);
+
+    //     return $return;
+    // }
+
+    /**
+     * Gets IP address.
+     */
+    public static function getGpsFromIp() {
+        $ip = '';
+        if (! empty($_SERVER['HTTP_CLIENT_IP'])) {
+            // to get shared ISP IP address
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (! empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            // check for IPs passing through proxy servers
+            // check if multiple IP addresses are set and take the first one
+            $ipList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            foreach ($ipList as $ip) {
+                if (! empty($ip)) {
+                    // if you prefer, you can check for valid IP address here
+                    $ip = $ip;
+                    break;
+                }
+            }
+        } else if (! empty($_SERVER['HTTP_X_FORWARDED'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED'];
+        } else if (! empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        } else if (! empty($_SERVER['HTTP_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_FORWARDED_FOR'];
+        } else if (! empty($_SERVER['HTTP_FORWARDED'])) {
+            $ip = $_SERVER['HTTP_FORWARDED'];
+        } else if (! empty($_SERVER['REMOTE_ADDR'])) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        
         $data = \Location::get($ip);
 
         $return = ['ip' => $ip, 'data' => $data];
         dd($return,$_SERVER);
 
         return $return;
-    }
-
-    /**
-     * Gets IP address.
-     */
-    public function getIpAddress() {
-        $ipAddress = '';
-        if (! empty($_SERVER['HTTP_CLIENT_IP'])) {
-            // to get shared ISP IP address
-            $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
-        } else if (! empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            // check for IPs passing through proxy servers
-            // check if multiple IP addresses are set and take the first one
-            $ipAddressList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            foreach ($ipAddressList as $ip) {
-                if (! empty($ip)) {
-                    // if you prefer, you can check for valid IP address here
-                    $ipAddress = $ip;
-                    break;
-                }
-            }
-        } else if (! empty($_SERVER['HTTP_X_FORWARDED'])) {
-            $ipAddress = $_SERVER['HTTP_X_FORWARDED'];
-        } else if (! empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
-            $ipAddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-        } else if (! empty($_SERVER['HTTP_FORWARDED_FOR'])) {
-            $ipAddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        } else if (! empty($_SERVER['HTTP_FORWARDED'])) {
-            $ipAddress = $_SERVER['HTTP_FORWARDED'];
-        } else if (! empty($_SERVER['REMOTE_ADDR'])) {
-            $ipAddress = $_SERVER['REMOTE_ADDR'];
-        }
-        return $ipAddress;
     }
 
     public static function getGeoLocation($latitude,$longitude){
