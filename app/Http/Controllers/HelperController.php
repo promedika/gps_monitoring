f429d40b3fe7163919968e5745aec8f59db1528f
andrian.dwiari <andrian@globalpromedika.co.id>
2022-10-27 10:51:13 +0700

    detect fake gps from gmaps api

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -12,50 +12,9 @@ class HelperController extends Controller
         $data = \Location::get($ip);
 
         $return = ['ip' => $ip, 'data' => $data];
-        // dd($return,$_SERVER);
 
         return $return;
     }
-
-    /**
-     * Gets IP address.
-     */
-    // public static function getGpsFromIp() {
-    //     $ip = '';
-    //     if (! empty($_SERVER['HTTP_CLIENT_IP'])) {
-    //         // to get shared ISP IP address
-    //         $ip = $_SERVER['HTTP_CLIENT_IP'];
-    //     } else if (! empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    //         // check for IPs passing through proxy servers
-    //         // check if multiple IP addresses are set and take the first one
-    //         $ipList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-    //         foreach ($ipList as $ip) {
-    //             if (! empty($ip)) {
-    //                 // if you prefer, you can check for valid IP address here
-    //                 $ip = $ip;
-    //                 break;
-    //             }
-    //         }
-    //     } else if (! empty($_SERVER['HTTP_X_FORWARDED'])) {
-    //         $ip = $_SERVER['HTTP_X_FORWARDED'];
-    //     } else if (! empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
-    //         $ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-    //     } else if (! empty($_SERVER['HTTP_FORWARDED_FOR'])) {
-    //         $ip = $_SERVER['HTTP_FORWARDED_FOR'];
-    //     } else if (! empty($_SERVER['HTTP_FORWARDED'])) {
-    //         $ip = $_SERVER['HTTP_FORWARDED'];
-    //     } else if (! empty($_SERVER['REMOTE_ADDR'])) {
-    //         $ip = $_SERVER['REMOTE_ADDR'];
-    //     }
-        
-    //     $ip = '120.188.7.30';
-    //     $data = \Location::get($ip);
-
-    //     $return = ['ip' => $ip, 'data' => $data];
-    //     dd($return,$_SERVER);
-
-    //     return $return;
-    // }
 
     public static function getGeoLocation($latitude,$longitude){
         if (!empty($latitude) && !empty($longitude)) {
@@ -63,15 +22,26 @@ class HelperController extends Controller
             $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&sensor=false&key='.env('GEOCODE_KEY');
             $geocodeFromLatLong = file_get_contents($url);
             $output = json_decode($geocodeFromLatLong);
+
+            $plus_code = $output->plus_code;
+            $results = $output->results[0];
             $status = $output->status;
 
+            $data = [
+                'plus_code' => $plus_code,
+                'results' => $results,
+                'status' => $status
+            ];
+
+            $global_code = substr(trim($data['plus_code']->global_code), 0, 6);
+
             //Get address from json data
-            $place_id = ($status=="OK") ? $output->results[0]->place_id : '';
+            $global_code = ($status=="OK") ? $global_code : '';
 
             //Return place_id of the given latitude and longitude
-            if (!empty($place_id)) {
+            if (!empty($global_code)) {
                 $return = [
-                    'place_id' => $place_id,
+                    'place_id' => $global_code,
                     'detail_location' => json_encode($output)
                 ];
                 return $return;
