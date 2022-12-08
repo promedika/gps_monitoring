7baaf43b34aef2e02fa1cfae16ea6dc2d98f9c41
andrian.dwiari <andrian@globalpromedika.co.id>
2022-12-08 10:52:49 +0700

    test debug ios

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -109,6 +109,7 @@ class AttendanceController extends Controller
 
         if (preg_match('/iPhone|iPod|iPad/', $_SERVER['HTTP_USER_AGENT'])) {
             /* This is iOS */
+            dd('ios');
             // get detail location from ip address
             $tmpDataIp = HelperController::getGpsFromIp();
 
@@ -120,6 +121,7 @@ class AttendanceController extends Controller
             $imgLocationLongitude = $dataIp['data']->longitude;
         }
         else {
+            dd('selain ios');
             try {
                 exif_read_data($tmp_path);
             } catch (\Throwable $th) {
