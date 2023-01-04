ab7b0026735357e0e8d03984cd53e52526d1bb97
andrian.dwiari <andrian@globalpromedika.co.id>
2023-01-04 09:36:33 +0700

    check ios / android / other

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -225,6 +225,21 @@
     <script>
         $(document).ready(function() {
 
+            const getMobileOS = () => {
+              const ua = navigator.userAgent
+              if (/android/i.test(ua)) {
+                return "Android"
+              }
+              else if ((/iPad|iPhone|iPod/.test(ua))
+                 || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)){
+                return "iOS"
+              }
+              return "Other"
+            }
+
+            const os = getMobileOS()
+            alert(os)
+
             function getGPS() {
                 if (navigator.geolocation) {  
                     navigator.geolocation.getCurrentPosition(showGPS, gpsError);
